5f9eaa5be92b2ca066319fafd6d3735a6785e5a9
nefertirii <nefertiri972@gmail.com>
2021-06-28 23:52:03 +0900

    Add cascade test
    영속성 전이(Cascade) 테스트
    Parent를 persist 할 때 Parent 의 Child 컬렉션도 같이 persist 해주는 기능
    Child를 Parent 에서만 유일하게 관리할 때 사용가능함(단일 소유자에 종속적일때 사용해야함)

---
+++ src/main/java/jpabasic/hellojpa/Parent.java
@@ -0,0 +1,49 @@
+package jpabasic.hellojpa;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.CascadeType.ALL;
+
+@Entity
+public class Parent {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "parent", cascade = ALL)
+    private List<Child> childList = new ArrayList<>();
+
+    // 연관관계 편의 메소드
+    public void addChild(Child child) {
+        childList.add(child);
+        child.setParent(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Child> getChildList() {
+        return childList;
+    }
+
+    public void setChildList(List<Child> childList) {
+        this.childList = childList;
+    }
+}
